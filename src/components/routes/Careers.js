8c454939ee520c208265306bad565a2177631019
Adam <adam.kilis@yahoo.com>
2020-04-03 23:25:40 +0200

    Map resize handler added, first changes in careers subpage

--- src/components/routes/Careers.js
+++ src/components/routes/Careers.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import polygon from '../../img/poly.png';
 
 function Careers() {
+  const [winWidth, setWinWidth] = useState(window.innerWidth);
+
+  function handleResize() {
+    setWinWidth(window.innerWidth)
+};
+
+    useEffect(() => {
+      window.addEventListener("resize", handleResize);
+      return () => {
+          window.removeEventListener("resize", handleResize);
+      }
+  }, [winWidth]);
+
+  const extraStyle = {
+    width: `${winWidth}px`
+  }
+
     return (
-      <div className="section-first-careers">
+      <div>
+        <section className="careers-section-1">
+          <div className="careers-section-1__title-wrapper">
+            <p className="careers-section-1__title-1">JOIN US</p>   
+          </div>  
+        </section>
+        <section className="careers-section-2">
+          <img className="careers-section-2__polygon" style={extraStyle} src={polygon} alt="pol1" />
+          <img className="careers-section-2__polygon rotate" style={extraStyle} src={polygon} alt="pol2" />
+        </section>
       </div>
     )
 }
--- src/components/routes/about/map/Map.js
+++ src/components/routes/about/map/Map.js
@@ -1,9 +1,22 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import L from 'leaflet';
 import  "../../../../../node_modules/leaflet.tilelayer.colorfilter/src/leaflet-tilelayer-colorfilter.min.js";
 import icn from '../../../../icons/Favorites/rocket.png';
 
-function Map({markerPosition}){
+function Map(){
+    const [width, setWidth] = useState(window.innerWidth)
+
+    function handleResize() {
+        setWidth(window.innerWidth)
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    }, [width]);
+
     const myIcon = L.icon({
                 iconUrl: icn,
                 iconSize: [25, 45],
@@ -13,10 +26,19 @@ function Map({markerPosition}){
                         ///CREATING MAP///
     const mapRef = useRef(null);
 
+    const lat1 = 37;
+    let long1 = () => {
+        if (width <= 767){
+            return -105
+        } else {
+            return -100
+        }
+    };
+
     useEffect(() =>{
         mapRef.current = L.map('map',{
-            center: [37, -115],
-            zoom:4,
+            center: [lat1, long1()],
+            zoom: `${(width <= 767) ? 3 : 4}`,
             layers: [
                 L.tileLayer.colorFilter('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
                 maxZoom: 15,
